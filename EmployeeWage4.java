d7a2cbdeccc41e274cff03d6ae6ef4213d8c00ef
gitanjali <gitaanjalide@gmail.com>
2022-07-14 00:58:10 +0530

    Part time and Full time Wages

---
+++ EmployeeWage4.java
@@ -0,0 +1,20 @@
+
+	public class EmployeeWage4 {
+	    public static void main(String[] arg) {
+	        System.out.println("Welcome To The Emp Wages Calculator Stimulator To Check Salary");
+
+	        int Full_time=8;
+	        int Part_time=4;
+	        int Wages_pr_hr =20;
+	        int Present = 1;
+	        double value = Math.floor(Math.random() * 10) % 2;
+	        if (value == Present) {
+	            System.out.println("Employee is Present");
+	            System.out.println("Daily wages of an Employee is : " +Full_time * Wages_pr_hr);
+	            System.out.println("Daily wages of an Employee is : " +Part_time * Wages_pr_hr);
+	        } else
+	            System.out.println("Employee is Absent");
+
+	        }
+	        }
+
