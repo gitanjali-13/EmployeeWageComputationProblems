309222c8a75f00fcfbccda4ea41790fd2d97b553
gitanjali <gitaanjalide@gmail.com>
2022-07-14 00:51:10 +0530

    check attendence

---
+++ checkAttendance.java
@@ -0,0 +1,19 @@
+
+public class checkAttendance {
+	    double check = Math.floor(Math.random() * 10) % 2;
+	    void isPresent(){
+	        if ( check == 1)
+	            System.out.println("Employee is Present !");
+	        else
+	            System.out.println("Employee is Absent !");
+	    }
+	}
+ class EmployeeWageComputationInJava {
+	    public static void main(String[] args){
+	        checkAttendance employee = new checkAttendance();
+	        System.out.println("Welcome to employee wage computation program on master branch");
+	        employee.isPresent();
+	    }
+	
+
+}
