a36f22d715967192a3df06f3fe4214f72dc41ff7
gitanjali <gitaanjalide@gmail.com>
2022-07-14 15:41:01 +0530

    display message

---
+++ src/com/bridgelabz/display/EmpWage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.display;
+
+public class EmpWage {
+	public static void main(){
+	System.out.println("Welcome To The Employee Wage Calculator to Check Employee Salary");
+}
+}
